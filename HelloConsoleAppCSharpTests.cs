e1b00b5897957f269e51626635d87607eae643ee
muzudho <muzudho1@gmail.com>
2026-03-28 23:58:29 +0900

    Ok: xUnit test

---
+++ HelloConsoleAppCSharpTests.cs
@@ -0,0 +1,32 @@
+﻿namespace HelloConsoleAppCSharpTests;
+
+using HelloConsoleAppCSharp.Infrastructure.Configuration;
+using Xunit;
+
+/// <summary>
+///     <pre>
+/// 単体テストの例。
+/// 
+///     - Grok の書いたものを調整しました。
+///     </pre>
+/// </summary>
+public class HelloConsoleAppCSharpTests
+{
+    /// <summary>
+    /// 2 と 3 を足したら 5 になることをテストする例。
+    /// </summary>
+    [Fact]
+    public void AppName_SetString_ReturnsAppName()
+    {
+        // Expected
+        var expected = "TestApp";
+
+
+        // Act
+        var appSettings = new MuzAppSettings();
+        appSettings.AppName = expected;
+
+        // Assert
+        Assert.Equal(expected, appSettings.AppName);
+    }
+}
